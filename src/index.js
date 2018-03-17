a9ca2ca88555fbf29c22d51425dac1cf0ffb3a9b
devildummy <cyrok.13@gmail.com>
2018-03-17 03:36:17 +0300

    Implement function Index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var zero=0;
+  var left = 0;
+  for(i=1;i<13;i++){
+  if (number>=Math.pow(5,i)){
+    left=number % Math.pow(5,i);
+    number=number - left;
+    zero+=number/Math.pow(5,i)
+  }
+}
+  return zero;
 }
