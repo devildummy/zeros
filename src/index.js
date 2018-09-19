b253937d2d0d8e9c18ad9cc220bcbb4c0becde05
devildummy <cyrok.13@gmail.com>
2018-09-20 02:33:30 +0300

    new version, sorry, but I'll peep at math forum '-_-

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 module.exports = function getZerosCount(number) {
-  var zero=0;
-  var left = 0;
-  for(i=1;i<13;i++){
-  if (number>=Math.pow(5,i)){
-    left=number % Math.pow(5,i);
-    number=number - left;
-    zero+=number/Math.pow(5,i)
+  let zeros=0;
+  let i=1;
+  do{
+    zeros+= (number - (number%Math.pow(5,i)))/Math.pow(5,i);
+    i++;
   }
-}
-  return zero;
-}
+  while (number>Math.pow(5,i));
+  return zeros;
+};
